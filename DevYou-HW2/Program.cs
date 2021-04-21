5169fdfda21a9d1d670ee24b85dd35710584c0eb
Eli <ezsto92@gmail.com>
2021-04-21 12:13:29 -0400

    Devyou HW2

---
+++ DevYou-HW2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DevYou_HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 1: Fahrenheit to Celsius
+            //Prompt your user for the degrees in Fahrenheit
+            Console.WriteLine("Enter the temperature in Fahrenheit");
+            double tempF = double.Parse(Console.ReadLine());
+            //Display the degrees in Fahrenheit and its equivalent in Celsius on the Console
+            double tempC = ((tempF - 32)*5)/9;
+            Console.WriteLine($"{tempC} is your temperature in celsius");
+
+            //Exercise 2: Sales Tax
+            //Prompt your user for the price of their product before taxes
+            Console.WriteLine("What is the price of your product before taxes?");
+            double prodBeforeTax = double.Parse(Console.ReadLine());
+            //Prompt the user to give you the sales tax
+            Console.WriteLine("Enter your sales tax in decimal format. Example: .06");
+            double userSalesTax = double.Parse(Console.ReadLine());
+            //Display the total cost by calculating the price plus the sales tax to the Console
+            double productWithTax = (prodBeforeTax * userSalesTax) + prodBeforeTax;
+            Console.WriteLine($"The total of your product with sales tax is {productWithTax}");
+        }
+    }
+}
